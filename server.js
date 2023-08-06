790423fc83478e6dea5016bb45b3f3f5c31fe0fc
Abhishek <abhishekpradhan931@gmail.com>
2023-08-07 03:38:03 +0530

    server.js updated

--- server.js
+++ server.js
@@ -15,8 +15,6 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
 
 require("./src/middlewares/passport")(passport);
 
